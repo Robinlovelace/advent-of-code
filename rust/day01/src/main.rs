669ed3a3c173aab1501cbc022773b85a584c7646
Robin Lovelace <rob00x@gmail.com>
2022-12-06 20:34:08 +0000

    Add total value of all food

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -13,5 +13,15 @@ fn read_file(path: &str) -> String {
 fn main() {
     // println!("Hello, world!");
     let res = read_file("input");
-    println!("{}", res);
+    // add the contents of each line:
+    let mut sum = 0;
+    for line in res.lines() {
+        // ignore empty lines:
+        if line.len() == 0 {
+            continue;
+        }
+        let num: i32 = line.parse().unwrap();
+        sum += num;
+    }
+    println!("{}", sum);
 }
