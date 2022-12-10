ecafd4ed98e881d6f8b63bf181ca65017c738e61
Robin Lovelace <rob00x@gmail.com>
2022-12-10 18:07:45 +0000

    No compiler warnins (unnused variable)

--- rust/day02/src/main.rs
+++ rust/day02/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     lookup_value.insert("Scissors".to_string(), 3);
     // Create a lookup table from letter A to C to Rock, Paper, Scissors:
     let mut lookup_item = HashMap::new();
+    // Note: This these three lookups are not needed for the result:
     lookup_item.insert("A".to_string(), "Rock".to_string());
     lookup_item.insert("B".to_string(), "Paper".to_string());
     lookup_item.insert("C".to_string(), "Scissors".to_string());
@@ -34,21 +35,31 @@ fn main() {
     lookup_win.insert("Rock Rock".to_string(), 3);
     lookup_win.insert("Paper Paper".to_string(), 3);
     lookup_win.insert("Scissors Scissors".to_string(), 3);
+
+    // Variable for total points:
+    let mut total = 0;
     
-    // Print the lookup table:
-    println!("{:?}", lookup_item);
+    // // Print the lookup table:
+    // println!("{:?}", lookup_item);
     // Read the file
     let contents = read_to_string("input.test").unwrap();
-    // Take the first line:
-    let first_line = contents.lines().next().unwrap();
-    // Split the line into words:
-    let words: Vec<String> = first_line.split_whitespace().map(|word| word.to_string()).collect();
-    // // Lookup items:
-    let item1 = &lookup_item[&words[0]];
-    let items: Vec<String> = words.iter().map(|word| lookup_item[word].to_string()).collect();
-    // // Lookup values:
-    let values: Vec<i32> = items.iter().map(|item| lookup_value[item]).collect();
-    // // Print the values:
-    println!("{:?}", items);
-    println!("{:?}", values);
+    // Iterate over the lines:
+    for line in contents.lines() {
+        // Split the line into words:
+        let words: Vec<String> = line.split_whitespace().map(|word| word.to_string()).collect();
+        // Lookup items:
+        let item1 = &lookup_item[&words[0]];
+        let item2 = &lookup_item[&words[1]];
+        // Lookup values:
+        // let value1 = lookup_value[item1];
+        let value2 = lookup_value[item2];
+        // Lookup the winner:
+        let winner = lookup_win[&format!("{} {}", item1, item2)];
+        // Print the winner:
+        println!("The winner is {} with {} points and a win value of {}", item2, value2, winner);
+        // println!("{} {} {} {} {}", item1, value1, item2, value2, winner);
+        total += winner + value2;
+    }
+    println!("Total points: {}", total);
+    
 }
