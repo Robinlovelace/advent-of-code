7577522e26c0bf8d83865fc4ca1d933f830e4db9
Robin Lovelace <rob00x@gmail.com>
2022-12-11 15:50:44 +0000

    Progress: identification of shared numbers

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    // Read in file:
+    let contents = read_to_string("input.test").unwrap();
+    // Print the contents:
+    // println!("{}", contents);
+    // get a single line:
+    let line = contents.lines().next().unwrap();
+    // Print the line:
+    println!("{}", line);
+    // Number of characters in the line:
+    let len = line.chars().count();
+    // Half of the length:
+    let half = len / 2;
+    // Split the line into two halves:
+    let (first, second) = line.split_at(half);
+    // Check for shared characters in the two halves:
+    for(i, c) in first.chars().enumerate() {
+        // For loop over characters in second half:
+        for (j, d) in second.chars().enumerate() {
+            // Check if the characters are the same:
+            if c == d {
+                // Print the characters:
+                println!("{} {}", c, d);
+                // Print the index of the characters:
+                println!("{} {}", i, j);
+            }
+        }
+    }
 }
