aaf4ea5accf9389951b49439f496457fb6eac447
Robin Lovelace <rob00x@gmail.com>
2022-12-11 16:55:36 +0000

    Working total calculated

--- rust/day03/src/main.rs
+++ rust/day03/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::read_to_string;
 use std::collections::HashMap;
+use std::fs::read_to_string;
 
 fn main() {
     // Total value:
@@ -15,42 +15,45 @@ fn main() {
         .cloned()
         .collect();
     // Print the characters:
-    println!("{:?}", characters);
+    // println!("{:?}", characters);
     // Hashmap:
     let mut char_vals: HashMap<char, usize> = HashMap::new();
     // For each character:
-    for(c, i) in characters.iter().enumerate() {
+    for (c, i) in characters.iter().enumerate() {
         char_vals.insert(*i, c + 1);
-        println!("{} {}", i, c + 1);
+        // println!("{} {}", i, c + 1);
     }
     // Read in file:
     let contents = read_to_string("input.test").unwrap();
     // Print the contents:
     // println!("{}", contents);
-    // get a single line:
-    let line = contents.lines().next().unwrap();
-    // Print the line:
-    // println!("{}", line);
-    // Number of characters in the line:
-    let len = line.chars().count();
-    // Half of the length:
-    let half = len / 2;
-    // Split the line into two halves:
-    let (first, second) = line.split_at(half);
-    // Check for shared characters in the two halves:
-    for(i, c) in first.chars().enumerate() {
-        // For loop over characters in second half:
-        for (j, d) in second.chars().enumerate() {
-            // Check if the characters are the same:
-            if c == d {
-                // Print the characters:
-                // println!("{} {}", c, d);
-                // Print the index of the characters:
-                // println!("{} {}", i, j);
-                // Print the value of the first matching character:
-                println!("{}", char_vals.get(&c).unwrap());
-                // Add the value to the total:
-                total += char_vals.get(&c).unwrap();
+    // // get a single line:
+    // let line = contents.lines().next().unwrap();
+    // For each line:
+    for line in contents.lines() {
+        // Print the line:
+        // println!("{}", line);
+        // Number of characters in the line:
+        let len = line.chars().count();
+        // Half of the length:
+        let half = len / 2;
+        // Split the line into two halves:
+        let (first, second) = line.split_at(half);
+        // Check for shared characters in the two halves:
+        for (i, c) in first.chars().enumerate() {
+            // For loop over characters in second half:
+            for (j, d) in second.chars().enumerate() {
+                // Check if the characters are the same:
+                if c == d {
+                    // Print the characters:
+                    // println!("{} {}", c, d);
+                    // Print the index of the characters:
+                    // println!("{} {}", i, j);
+                    // Print the value of the first matching character:
+                    // println!("{}", char_vals.get(&c).unwrap());
+                    // Add the value to the total:
+                    total += char_vals.get(&c).unwrap();
+                }
             }
         }
     }
